16e0ff8e464388b573c12fda32536832013b5c00
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2024-01-19 19:23:13 +0000

    Sidebar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,10 +1,13 @@
 import '../App.css';
+import {Link } from 'react-router-dom'
 
 function SideBar() {
     return (
         <div className="right-side">
             <div className="tabs">
-              
+                <Link>
+                 
+                </Link>
             </div>
         </div>
     );
